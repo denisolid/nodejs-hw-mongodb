2ff4364c74fdb212ac229f845d6759cd324d4782
denisolid <dkolisnichienko321@gmail.com>
2024-09-25 18:39:49 +0000

    validation schema

---
+++ src/validation/contacts.js
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.number().integer().min(6).max(12).required(),
+  email: Joi.string().min(3).max(20).required(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .required(),
+  isFavourite: Joi.boolean().required().default(false),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.number().integer().min(6).max(12),
+  email: Joi.string().min(3).max(20),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+  isFavourite: Joi.boolean(),
+});
