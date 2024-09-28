6274bb81caabd64333aeec19975970a7d170b018
denisolid <dkolisnichienko321@gmail.com>
2024-09-28 22:27:11 +0000

    pagination filtering and sorting provided

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ export const setupServer = () => {
     });
   });
 
-  app.use(contactsRouter); // Додаємо роутер до app як middleware
+  app.use(contactsRouter);
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
@@ -46,67 +46,3 @@ export const setupServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
-
-// export const setupServer = () => {
-//   const app = express();
-
-//   app.use(express.json());
-//   app.use(cors());
-
-//   app.use(
-//     pino({
-//       transport: {
-//         target: 'pino-pretty',
-//       },
-//     }),
-//   );
-
-//   app.get('/', (req, res) => {
-//     res.json({
-//       message: 'Hello world!',
-//     });
-//   });
-//   app.get('/students', async (req, res) => {
-//     const students = await getAllStudents();
-//     console.log(students);
-
-//     res.status(200).json({
-//       message: 'Successfully found contacts!',
-//       data: students,
-//     });
-//   });
-
-//   app.get('/students/:studentId', async (req, res, next) => {
-//     const { studentId } = req.params;
-//     const student = await getStudentById(studentId);
-
-//     if (!student) {
-//       res.status(404).json({
-//         message: `Contact not found ${studentId}`,
-//       });
-//       return;
-//     }
-
-//     res.status(200).json({
-//       data: student,
-//       message: `Successfully found contact with id ${studentId}!`,
-//     });
-//   });
-
-//   app.use('*', (req, res, next) => {
-//     res.status(404).json({
-//       message: 'Not found',
-//     });
-//   });
-
-//   app.use((err, req, res, next) => {
-//     res.status(500).json({
-//       message: 'Something went wrong',
-//       error: err.message,
-//     });
-//   });
-
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-// };
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,37 @@
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isContactType = (contactType) =>
+    ['work', 'home', 'personal'].includes(contactType);
+
+  if (isContactType(contactType)) return contactType;
+};
+
+const parseIsFavourite = (isFavourite) => {
+  const isBoolean = typeof isFavourite === 'boolean';
+  if (isBoolean) return isFavourite;
+};
+
+const parseNumber = (number) => {
+  const isString = typeof number === 'string';
+  if (!isString) return;
+
+  const parsedNumber = parseInt(number);
+  if (Number.isNaN(parsedNumber)) {
+    return;
+  }
+
+  return parsedNumber;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parsedContactType = parseContactType(contactType);
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+  return {
+    contactType: parsedContactType,
+    isFavourite: parsedIsFavourite,
+  };
+};
