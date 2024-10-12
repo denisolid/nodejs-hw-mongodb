01dc9254e9c8ead54426e718eb88cdb652db3452
denisolid <dkolisnichienko321@gmail.com>
2024-10-12 13:27:37 +0000

    phone number fixed

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().integer().min(6).max(12).required(),
+  phoneNumber: Joi.string().min(6).max(12).required(),
   email: Joi.string().min(3).max(20).required(),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
